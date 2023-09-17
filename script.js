e919c5dd22c2d8b6773676aa7a4d07549a514650
Falze Labib <fazle.labib@gmail.com>
2023-09-17 23:01:25 +0600

    Add total average score functionality

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-let subject = document.querySelectorAll('.subject')
-let title = document.querySelectorAll('.subject .title p')
-let icon = document.querySelectorAll('.subject .title img')
-let marks = document.querySelectorAll('.subject .marks .obtained')
+let subject = document.querySelectorAll('.subject');
+let title = document.querySelectorAll('.subject .title p');
+let icon = document.querySelectorAll('.subject .title img');
+let marks = document.querySelectorAll('.subject .marks .obtained');
+let totalAvg = document.querySelector('.results-card .circle h1');
+
 
 function populate() {
     fetch('data.json').then((response) => {
         response.json().then((subjects) => {
+            totalScore = 0;
             for (let i = 0; i < subject.length; i++) {
-                icon[i].src = subjects[i].icon
-                title[i].innerHTML = subjects[i].category
-                marks[i].innerHTML = subjects[i].score
+                icon[i].src = subjects[i].icon;
+                title[i].innerHTML = subjects[i].category;
+                marks[i].innerHTML = subjects[i].score;
+                totalScore += subjects[i].score;
             }
+            totalAvg.innerHTML = Math.round(totalScore / subject.length);
         })
     })
+
 }
 
-populate()
+populate();
